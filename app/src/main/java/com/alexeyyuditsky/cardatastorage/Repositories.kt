80eb50aced21cb805c96f3d62f24359a53b0792c
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2023-04-13 19:43:20 +0300

    init

---
+++ app/src/main/java/com/alexeyyuditsky/cardatastorage/Repositories.kt
@@ -0,0 +1,9 @@
+package com.alexeyyuditsky.cardatastorage
+
+import android.content.Context
+
+object Repositories {
+
+
+
+}
---
+++ app/src/main/java/com/alexeyyuditsky/cardatastorage/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.alexeyyuditsky.cardatastorage.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexeyyuditsky.cardatastorage.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
